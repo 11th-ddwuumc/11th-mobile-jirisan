66af7684cb2b27d8a832f396af202d10fb96fe8c
지리산불도저 <qoavk2002@gmail.com>
2026-09-17 16:09:10 +0900

    fix: 스타트 스크린 위젯 테스트 제목 문자열 수정

--- movielog/test/widget_test.dart
+++ movielog/test/widget_test.dart
@@ -16,7 +16,7 @@ void main() {
   ) async {
     await tester.pumpWidget(const MovieLogApp());
 
-    expect(find.text('영화의 순간을 기록하세요'), findsOneWidget);
+    expect(find.text('영화의 순간을\n기록하세요'), findsOneWidget);
     expect(find.text('시작하기'), findsOneWidget);
     expect(find.byIcon(Icons.movie_outlined), findsOneWidget);
   });
